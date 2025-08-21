f8ac632e66869cd4c7aee0c22f32f9e662bff017
Murat Turan <89749358+m-turan@users.noreply.github.com>
2025-08-21 20:31:38 +0300

    Update hosting_config.py

--- hosting_config.py
+++ hosting_config.py
@@ -8,7 +8,7 @@ FTP_PASS = "c_bJ!-PGMwG57#Hx"             # FTP şifresi
 FTP_PATH = "/public_html/yasinxml/"     # FTP yolu (genellikle /public_html/)
 
 # Dosya Adı
-XML_DOSYA_ADI = "tumurunler.xml"
+XML_DOSYA_ADI = "tumurunler2.xml"
 
 # Güvenlik Notu: Bu dosyayı git'e commit etmeyin!
 # Gerçek şifrelerinizi burada tutmayın, production'da environment variables kullanın 
