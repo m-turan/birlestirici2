3cd75b16f4a7b7b4122a33b67127f3fa9924c306
Murat Turan <89749358+m-turan@users.noreply.github.com>
2025-08-21 20:21:59 +0300

    aa
    aa

---
+++ hosting_config.py
@@ -0,0 +1,14 @@
+# Hosting Konfigürasyon Dosyası
+# Bu dosyayı kendi hosting bilgilerinizle güncelleyin
+
+# FTP Bilgileri
+FTP_HOST = "ftp.eterella.com"  # FTP sunucu adresi
+FTP_USER = "windamdx"     # FTP kullanıcı adı
+FTP_PASS = "c_bJ!-PGMwG57#Hx"             # FTP şifresi
+FTP_PATH = "/public_html/yasinxml/"     # FTP yolu (genellikle /public_html/)
+
+# Dosya Adı
+XML_DOSYA_ADI = "tumurunler.xml"
+
+# Güvenlik Notu: Bu dosyayı git'e commit etmeyin!
+# Gerçek şifrelerinizi burada tutmayın, production'da environment variables kullanın 
